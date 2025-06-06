d6bf6bd29cf4a2da091ee10bfc71f06fbffcd36a
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-06 15:34:48 -0500

    feat(main):Add application resources

---
+++ AccountSubscription-Microservice/AccountSubscription/Application/Resources/SubscriptionResource.cs
@@ -0,0 +1,25 @@
+﻿using DittoBox.API.AccountSubscription.Domain.Models.Entities;
+using DittoBox.API.AccountSubscription.Domain.Models.ValueObjects;
+
+namespace DittoBox.API.AccountSubscription.Application.Resources
+{
+	public record SubscriptionResource(
+		int Id,
+		int AccountId,
+		string Tier,
+		DateOnly PaymentDate,
+		int SubscriptionStatusId,
+		DateOnly LastPaidPeriod
+	)
+	{
+		public static SubscriptionResource FromSubscription(Subscription subscription)
+		{
+			return new SubscriptionResource(subscription.Id,
+				subscription.AccountId,
+				((SubscriptionTier)subscription.TierId).ToString(),
+				subscription.PaymentDate,
+				subscription.SubscriptionStatusId,
+				subscription.LastPaidPeriod);
+		}
+	}
+}
