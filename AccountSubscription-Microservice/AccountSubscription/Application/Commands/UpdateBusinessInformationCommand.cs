8452960045a72d704a320a6c5f9432a706a89d15
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-06 15:33:04 -0500

    feat(main):Add application commands

---
+++ AccountSubscription-Microservice/AccountSubscription/Application/Commands/UpdateBusinessInformationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DittoBox.API.AccountSubscription.Application.Commands
+{
+    public record UpdateBusinessInformationCommand
+    (
+        [Required] int AccountId,
+        [Required] string BusinessName,
+        [Required] string BusinessId
+        );
+}
